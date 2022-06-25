a4a16484a44684e9760cd6099859182596d53fdb
SAMEER TRIMADE <sameer.trimade@mountblue.tech>
2022-06-26 03:21:25 +0530

    first commit

---
+++ src/components/Banner/Login.js
@@ -0,0 +1,9 @@
+import {React, Button} from "react";
+
+function Login(){
+    return(
+        <Button href="www.youtube.com" target="_blank" color="white">Login</Button>
+    )
+}
+
+export default Login
